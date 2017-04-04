08eda6a1655f0056c317ee4776ad1b6784738ce3
Heather Banks <heather.banks03@gmail.com>
2017-04-03 23:22:37 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		concat = require('gulp-concat'),
+		jshint = require('gulp-jshint'),
+		autoprefixer = require('gulp-autoprefixer');
+
+// here we define the list of things to happen when we run gulp styles
+gulp.task('styles', function() {
+  return gulp.src('wp-content/themes/_jax/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('wp-content/themes/_jax/'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('wp-content/themes/_jax/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('wp-content/themes/_jax/sass/*.scss', ['styles']);
+	gulp.watch('wp-content/themes/_jax/js/*.js', ['jshint']);
+});
+
+gulp.task('default', ['styles', 'jshint', 'watch']);
