4c1d004e9083319d3f2167ad6b39b86993c27fb1
Heather Banks <heather.banks03@gmail.com>
2017-04-16 23:10:01 -0400

    updated gulp workflow to add css sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ var gulp = require('gulp'),
 		sass = require('gulp-sass'),
 		concat = require('gulp-concat'),
 		jshint = require('gulp-jshint'),
-		autoprefixer = require('gulp-autoprefixer');
+		autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // here we define the list of things to happen when we run gulp styles
 gulp.task('styles', function() {
-  return gulp.src('wp-content/themes/_jax/sass/*.scss')
+  return gulp.src(['wp-content/themes/_jax/sass/**/*.scss'], { base: 'wp-content/themes/_jax/sass/'})
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.css'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('wp-content/themes/_jax/'));
 });
 
@@ -21,7 +23,7 @@ gulp.task('jshint', function() {
 });
 
 gulp.task('watch', function(){
-	gulp.watch('wp-content/themes/_jax/sass/*.scss', ['styles']);
+	gulp.watch('wp-content/themes/_jax/sass/**/*.scss', ['styles']);
 	gulp.watch('wp-content/themes/_jax/js/*.js', ['jshint']);
 });
 
